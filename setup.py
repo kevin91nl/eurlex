e3f9939adcd3af59a9b2e13b9437c99d44c5496c
kevin91nl <kevin91nl@gmail.com>
2022-07-06 17:56:29 +0200

    Bugfix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="eurlex",
-    version="0.1.2",
+    version="0.1.3",
     author="K.M.J. Jacobs",
     author_email="mail@kevinjacobs.nl",
     description="An EUR-Lex parser for Python.",
