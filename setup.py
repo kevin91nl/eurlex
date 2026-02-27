64f59bb1c3225af0fbef4e6969e7e0477692587e
Kevin <3481474+kevin91nl@users.noreply.github.com>
2026-02-27 11:57:07 +0100

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="eurlex",
-    version="0.1.4",
+    version="0.1.5",
     author="K.M.J. Jacobs",
     author_email="kmj.jacobs@maastrichtuniversity.nl",
     description="An EUR-Lex parser for Python.",
