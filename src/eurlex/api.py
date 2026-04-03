12534e530e59ba3238bcc2ec33c4408336d7d9d5
Kevin <3481474+kevin91nl@users.noreply.github.com>
2026-04-03 16:04:13 +0200

    Remove deprecated api shim

--- src/eurlex/api.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility wrapper for the EUR-Lex public API.
-
-New code should import from `eurlex.public` or `eurlex`.
-"""
-
-from eurlex.public import *  # noqa: F401,F403
-from eurlex.public import __all__ as __all__
